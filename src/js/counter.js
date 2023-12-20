f9bfe5bb8017dc6da90e675f0b8020d70ca40bee
Suphakit P <me@suphakit.net>
2023-12-21 00:46:36 +0700

    feat<script>: added counter script

---
+++ src/js/counter.js
@@ -0,0 +1,76 @@
+const time = document.querySelector("#time");
+
+const startButton = document.querySelector(".controller > #start");
+const pauseButton = document.querySelector(".controller > #pause");
+const resetButton = document.querySelector(".controller > #end");
+
+const timeBased = 60000;
+
+let timeDuration = timeBased;
+let min;
+let sec;
+
+function setDisplay() {
+  min = Math.floor((timeDuration % (1000 * 60 * 60)) / (1000 * 60)).toString();
+  sec = Math.floor((timeDuration % (1000 * 60)) / 1000).toString();
+
+  time.innerHTML = `<div class="timeContainer">
+  <h1 style="transform: scale(2, 2)">
+    ${min.length < 2 ? 0 + min : min} : ${sec.length < 2 ? 0 + sec : sec}
+  </h1>
+  </div>
+`;
+}
+
+setDisplay();
+let count;
+
+function timer() {
+  count = setInterval(() => {
+    min = Math.floor(
+      (timeDuration % (1000 * 60 * 60)) / (1000 * 60)
+    ).toString();
+    sec = Math.floor((timeDuration % (1000 * 60)) / 1000).toString();
+
+    if (timeDuration <= 0) {
+      time.innerHTML = `<div class="timeContainer">
+    <h1 style="transform: scale(2, 2);">
+      Time UP!
+    </h1>
+  </div>`;
+
+      clearInterval(count);
+    } else {
+      time.innerHTML = `<div class="timeContainer">
+<h1 style="transform: scale(2, 2)">
+  ${min.length < 2 ? 0 + min : min} : ${sec.length < 2 ? 0 + sec : sec}
+</h1>
+</div>
+`;
+    }
+
+    timeDuration = timeDuration - 1000;
+  }, 1000);
+}
+
+function start() {
+  startButton.classList.add("hidden");
+  pauseButton.classList.remove("hidden");
+  timer();
+}
+
+function pause() {
+  pauseButton.classList.add("hidden");
+  startButton.classList.remove("hidden");
+  clearInterval(count);
+}
+
+function reset() {
+  pauseButton.classList.add("hidden");
+  startButton.classList.remove("hidden");
+
+  timeDuration = timeBased;
+  setDisplay();
+
+  clearInterval(count);
+}
