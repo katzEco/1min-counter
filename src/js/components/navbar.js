34df7e446d296dc684213251603ef8391a1e7062
Suphakit P <me@suphakit.net>
2023-12-20 23:42:14 +0700

    feat<components>: navbar setting up

---
+++ src/js/components/navbar.js
@@ -0,0 +1,9 @@
+const navTag = document.querySelector("#navbar");
+
+navTag.innerHTML = `<div class="left">
+  <a href="./" class="nav-button">1Min Counter</a>
+</div>
+<div class="right">
+  <a href="./about" class="nav-button">About</a>
+</div>
+`;
