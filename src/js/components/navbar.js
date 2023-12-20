f378803a1c040db5c997516ba1bd84ae65b6a919
Suphakit P <me@suphakit.net>
2023-12-21 00:54:17 +0700

    feat<components>: hide about page

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -4,6 +4,6 @@ navTag.innerHTML = `<div class="left">
   <a href="./" class="nav-button">1Min Counter</a>
 </div>
 <div class="right">
-  <a href="./about" class="nav-button">About</a>
+  <!-- <a href="./about" class="nav-button">About</a> -->
 </div>
 `;
